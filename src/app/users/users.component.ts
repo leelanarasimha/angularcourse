ecff9bebf80852a7633e32c0ff495281cb9472cb
Leela Narasimha <leelanrp@qualminds.com>
2020-09-11 22:42:27 +0530

    sdsad

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onCategoriesClick() {
+    //perform some logic
+    //navigate to page
+
+    this.router.navigate(['/categories']);
   }
-
 }
