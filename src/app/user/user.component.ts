ba318c67f84d4a24101ec74591735f754c3671f6
Leela Narasimha <leelanrp@qualminds.com>
2020-09-12 00:25:56 +0530

    sadsd

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +8,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user: { id: string; name: string };
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.user = {
@@ -20,6 +20,13 @@ export class UserComponent implements OnInit {
         id: data['id'],
         name: data['name'],
       };
+    });
+  }
+
+  getRamaDetails() {
+    this.router.navigate(['/users', 2, 'Rama'], {
+      queryParams: { page: 1, search: 'Leela' },
+      fragment: 'loading',
     });
   }
 }
