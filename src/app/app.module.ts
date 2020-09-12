4f83d4ac0b6f4bd21331153e310b8bb950ec7c9d
Leela Narasimha <leelanrp@qualminds.com>
2020-09-12 14:37:42 +0530

    dasdas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,11 @@ import { UserComponent } from './user/user.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
 
   { path: 'categories', component: CategoriesComponent },
 ];
