cdfa5c999f56ab4b34bcf732a8f7e6a41d52eadb
Leela Narasimha <leelanrp@qualminds.com>
2020-09-14 23:34:51 +0530

    asdsa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -15,10 +16,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.routeSubscription = this.route.data.subscribe((data: Data) => {
       console.log(data);
+    }, error => {
+      console.log(error);
     });
 
-    this.intervalSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    let customObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error('count is greater than 3')
+        }
+
+        if (count > 2) {
+          observer.complete();
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.intervalSubscription = customObservable.pipe(map((data: number) => {
+
+      return 'count is ' + (data + 1);
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('complete');
     })
   }
 
