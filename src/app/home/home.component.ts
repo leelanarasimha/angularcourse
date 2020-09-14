f4c7c1aeccdaa97e5d9eb697d0485d730086d819
Leela Narasimha <leelanrp@qualminds.com>
2020-09-14 19:56:31 +0530

    asasa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+export class HomeComponent implements OnInit, OnDestroy {
+  intervalSubscription: Subscription;
+  routeSubscription: Subscription;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.data.subscribe((data: Data) => {
+    this.routeSubscription = this.route.data.subscribe((data: Data) => {
       console.log(data);
     });
+
+    this.intervalSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+
+  ngOnDestroy() {
+    this.intervalSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
   }
 }
