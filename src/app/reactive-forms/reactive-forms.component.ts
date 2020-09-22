049c343d4fe9dc375563a7a705c7a41b947d0467
Leela Narasimha <leelanrp@qualminds.com>
2020-09-22 16:09:17 +0530

    adasds

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-forms',
+
   templateUrl: './reactive-forms.component.html',
   styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
   genders = ['male', 'female'];
+  restrictedNames = ['Leela'];
   signUpForm: FormGroup;
   constructor() { }
 
@@ -18,8 +21,8 @@ export class ReactiveFormsComponent implements OnInit {
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'username': new FormControl(null, [Validators.required, this.isRestrictedNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], [this.isRestrictedEmails]),
       }),
 
       'gender': new FormControl('female'),
@@ -30,6 +33,29 @@ export class ReactiveFormsComponent implements OnInit {
   onSubmit() {
     console.log(this.signUpForm);
   }
+
+  isRestrictedNames(control: FormControl): { [s: string]: boolean } {
+    if (this.restrictedNames.includes(control.value)) {
+      return { nameIsRestricted: true };
+    }
+    return null;
+  }
+
+  isRestrictedEmails(control: FormControl): Promise<any> | Observable<any> {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsRestricted: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000)
+    });
+    return promise;
+
+  }
+
+
 
   onAddHobby() {
     const control = new FormControl(null, [Validators.required]);
