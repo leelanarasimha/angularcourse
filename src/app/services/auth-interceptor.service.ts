3a8ba0808bfee06170a48b77705c612b475b780a
Leela Narasimha <leelanrp@qualminds.com>
2020-10-03 13:03:57 +0530

    Added response interceptors

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,8 +1,10 @@
 import {
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -10,6 +12,14 @@ export class AuthInterceptorService implements HttpInterceptor {
       headers: req.headers.append('auth', 'abc'),
       params: req.params.append('hai', 'hello world'),
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log(event);
+        console.log('Response from interceptor');
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
